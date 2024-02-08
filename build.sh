c3643ccdfdb2e471cd64dd1fa821742f38e6c75f
林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>
2024-02-09 01:59:46 +0800

    build: feat: Implement software sources preparation logic
    Signed-off-by: 林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>

--- build.sh
+++ build.sh
@@ -16,6 +16,13 @@ init(){
         exit 1
     fi
 
+    if ! prepare_software_sources; then
+        printf \
+            'Error: Unable to prepare the software sources.\n' \
+            1>&2
+        exit 2
+    fi
+
     print_progress \
         'Operation completed without errors.'
 }
@@ -87,6 +94,157 @@ check_runtime_parameters(){
             'Error: This program requires to be run as the superuser(root).\n' \
             1>&2
         return 2
+    fi
+}
+
+prepare_software_sources(){
+    print_progress 'Preparing software sources...'
+
+    local apt_archive_cache_mtime_epoch
+    if ! apt_archive_cache_mtime_epoch="$(
+        stat \
+            --format=%Y \
+            /var/cache/apt/archives
+        )"; then
+        printf \
+            'Error: Unable to query the modification time of the APT software sources cache directory.\n' \
+            1>&2
+        return 2
+    fi
+
+    local current_time_epoch
+    if ! current_time_epoch="$(
+        date +%s
+        )"; then
+        printf \
+            'Error: Unable to query the current time.\n' \
+            1>&2
+        return 2
+    fi
+
+    if test "$((current_time_epoch - apt_archive_cache_mtime_epoch))" -ge 86400; then
+        printf \
+            'Info: Refreshing the APT local package cache...\n'
+        if ! apt-get update; then
+            printf \
+                'Error: Unable to refresh the APT local package cache.\n' \
+                1>&2
+            return 2
+        fi
+    fi
+
+    # Silence warnings regarding unavailable debconf frontends
+    export DEBIAN_FRONTEND=noninteractive
+
+    base_runtime_dependency_pkgs=(
+        wget
+    )
+    if ! dpkg -s "${base_runtime_dependency_pkgs[@]}" &>/dev/null; then
+        printf \
+            'Info: Installing base runtime dependency packages...\n'
+        if ! \
+            apt-get install \
+                -y \
+                "${base_runtime_dependency_pkgs[@]}"; then
+            printf \
+                'Error: Unable to install the base runtime dependency packages.\n' \
+                1>&2
+            return 2
+        fi
+    fi
+
+    if ! test -v CI; then
+        printf \
+            'Info: Detecting local region code...\n'
+        wget_opts=(
+            # Output to the standard output device
+            --output-document=-
+
+            # Don't output debug messages
+            --quiet
+        )
+        if ! ip_reverse_lookup_service_response="$(
+                wget \
+                    "${wget_opts[@]}" \
+                    https://ipinfo.io/json
+            )"; then
+            printf \
+                'Warning: Unable to detect the local region code(IP address reverse lookup service not available), falling back to default.\n' \
+                1>&2
+            region_code=
+        else
+            grep_opts=(
+                --perl-regexp
+                --only-matching
+            )
+            if ! region_code="$(
+                grep \
+                    "${grep_opts[@]}" \
+                    '(?<="country": ")[[:alpha:]]+' \
+                    <<<"${ip_reverse_lookup_service_response}"
+                )"; then
+                printf \
+                    'Warning: Unable to query the local region code, falling back to default.\n' \
+                    1>&2
+                region_code=
+            else
+                printf \
+                    'Info: Local region code determined to be "%s"\n' \
+                    "${region_code}"
+            fi
+        fi
+
+        if test -n "${region_code}"; then
+            # The returned region code is capitalized, fixing it.
+            region_code="${region_code,,*}"
+
+            printf \
+                'Info: Checking whether the local Ubuntu archive mirror exists...\n'
+            wget_opts=(
+                # Output to the standard output device
+                --output-document=-
+
+                # Don't output debug messages
+                --quiet
+            )
+            if ! \
+                wget \
+                    "${wget_opts[@]}" \
+                    "http://${region_code}.archive.ubuntu.com" \
+                    >/dev/null; then
+                printf \
+                    "Warning: The local Ubuntu archive mirror doesn't seem to exist, falling back to default...\\n"
+                region_code=
+            else
+                printf \
+                    'Info: The local Ubuntu archive mirror service seems to be available, using it.\n'
+            fi
+        fi
+
+        if test -n "${region_code}" \
+            && ! grep -q "${region_code}.archive.u" /etc/apt/sources.list; then
+            printf \
+                'Info: Switching to use the local APT software repository mirror...\n'
+            if ! \
+                sed \
+                    --in-place \
+                    "s@//archive.u@//${region_code}.archive.u@g" \
+                    /etc/apt/sources.list; then
+                printf \
+                    'Error: Unable to switch to use the local APT software repository mirror.\n' \
+                    1>&2
+                return 2
+            fi
+
+            printf \
+                'Info: Refreshing the local APT software archive cache...\n'
+            if ! apt-get update; then
+                printf \
+                    'Error: Unable to refresh the local APT software archive cache.\n' \
+                    1>&2
+                return 2
+            fi
+        fi
     fi
 }
 
